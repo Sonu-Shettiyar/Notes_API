18c17f507c756bc9f53d2867494d56d8c37d270f
Sonu-Shettiyar <sonubadmash003@gmail.com>
2023-06-11 16:48:03 +0530

    cors_fixed

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const { notesRouter } = require("./routes/notes.route");
 const app = express();
 app.use(express.json());
 require("dotenv").config();
+
 const cors = require("cors");
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
+
 app.use("/users", userRouter);
 app.use("/notes",notesRouter)
 
