5eebdd2e4d8c2ecf6a7248a353b8b3c8ff90aff2
Sonu-Shettiyar <sonubadmash003@gmail.com>
2023-06-11 17:03:24 +0530

    done_cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const corsOptions = {
   
   app.use(cors(corsOptions));  
   app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'https://verdant-blini-4070cf.netlify.app');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       next();
